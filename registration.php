bda0c028d5f6e4555dc905db6b941f7bddd65ee1
shbabel <yourmediafriend@gmail.com>
2018-11-19 15:11:29 +0000

    Added Luma Theme as base

--- registration.php
+++ registration.php
@@ -1,16 +1,11 @@
 <?php
-// /**
-//  * Copyright © 2017
-//  */
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::THEME,
     'frontend/Shag/default',
     __DIR__
-);
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'shag_en_us',
-    __DIR__ . '/language-pack/en_US'
 );
